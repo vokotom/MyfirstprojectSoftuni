2f5e4bcde5a2f24e951f6e82585e9937825a1526
Victor Tomov <110666539+vokotom@users.noreply.github.com>
2022-10-06 19:07:14 +0300

    Update Repainting.py

--- Repainting.py
+++ Repainting.py
@@ -1,15 +1,23 @@
-nylon_count = float(input())
-paint_count = float(input())
-paint_thinner = float(input())
-hours = float(input())
+nylon = int(input())
+paint = int(input())
+thinner = int(input())
+hours = int(input())
 
-nylon_price = (nylon_count+2) * 1.50
-paint_price = paint_count * 15.95
-paint_thinner_price = paint_thinner * 5.00
-bags_price = 0.40
+price_nylon = 1.50
+price_paint = 14.50
+price_thinner = 5
+price_bags = 0.40
 
+extra_nylon = nylon + 2
+extra_paint = paint + (paint * 10 / 100)
 
-all_price = nylon_price + paint_price + paint_thinner_price + bags_price
-total_summ = (all_price * 0.30)  * hours
+final_price_nylon = extra_nylon * 1.50
+final_price_paint = extra_paint * 14.50
+final_price_thinner = thinner * 5
 
-print(total_summ)
+mat_price = final_price_paint + final_price_thinner + final_price_nylon + price_bags
+work_price = hours * (mat_price * 30 / 100)
+
+total_price = mat_price + work_price
+
+print(total_price)
